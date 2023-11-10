36038cf615612c6bc63c762b04af9ca0b2b3e4bb
22-bottle <gmlquddl67@naver.com>
2023-11-10 17:50:58 +0900

    check

--- enjoy-trip/src/components/views/TheUserView.vue
+++ enjoy-trip/src/components/views/TheUserView.vue
@@ -5,37 +5,30 @@ const userInfo = ref(true);
 </script>
 
 <template>
+  <!-- session에 userInfo 객체 없는 경우(로그인 X) -->
+  <!-- session에 userInfo 객체 있는 경우(로그인 O) -->
+  <!-- session 안쓰고 테스트 -->
+  <template v-if="!userInfo">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <router-link to="/login" class="nav-link">로그인</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link to="/regist" class="nav-link">회원가입</router-link>
+      </li>
+    </ul>
+  </template>
 
-<<<<<<< HEAD
-    <!-- session에 userInfo 객체 없는 경우(로그인 X) -->
-    <!-- session에 userInfo 객체 있는 경우(로그인 O) -->
-    <!-- session 안쓰고 테스트 -->
-    <template v-if="!userInfo">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <router-link to="/login" class="nav-link">로그인</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link to="/regist" class="nav-link">회원가입</router-link>
-        </li>
-      </ul>
-    </template>
-
-    <template v-else>
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <router-link :to="{ name: mypage, params: {} }" class="nav-link"
-            >${userInfo.userId }님 반가워요!</router-link
-          >
-        </li>
-        <li class="nav-item">
-          <router-link to="/logout" class="nav-link">로그아웃</router-link>
-        </li>
-      </ul>
-    </template>
-  </div>
-=======
->>>>>>> 686e539de68067c15a2de72f8fd8b4456d593636
+  <template v-else>
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <router-link :to="{ name: mypage, params: {} }" class="nav-link">${userInfo.userId }님 반가워요!</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link to="/logout" class="nav-link">로그아웃</router-link>
+      </li>
+    </ul>
+  </template>
 </template>
 
 <style scoped></style>
